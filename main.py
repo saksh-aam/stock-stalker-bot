ee45d389e0c6b65871098a1cdfde0a7c87024e6a
saksh-aam <80443254+saksh-aam@users.noreply.github.com>
2022-01-04 00:57:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 from cogs.details_fun import column_name
 
 # Keys and Global variables
-Bot=commands.Bot(command_prefix="-")
+Bot=commands.Bot(command_prefix="!rs")
 DiscordComponents(Bot)
 image=discord.File("./datafiles/test.png")
 
@@ -23,40 +23,39 @@ async def helpdetails(ctx):
     
 @Bot.command()
 async def helpdetials(ctx):
-      await ctx.send("To get started you got to be ready with **Security Ids** of stocks! \n\n"
-      "**Stocks specific details**:\n"
-      f"{', '.join(column_name)}\n\n"
-      "`For e.g- $issuename ABB `\n\n"
-
-      "**Aggregate details**:\n"
-      "Complete Detail of stock\n"
-      "List of Sectors\n"
-      "List of Igroup Name\n"
-      "List of Isubgroup Name\n"
-      "All the companies in particular sector\n\n"
-      "Type in `-help` to know the syntax/ command",
-      )
+    await ctx.send("To get started you got to be ready with **Security Ids** of stocks! \n\n"
+    "**Stocks specific details**:\n"
+    f"{', '.join(column_name)}\n\n"
+    "`For e.g- $issuename ABB `\n\n"
+    "**Aggregate details**:\n"
+    "Complete Detail of stock\n"
+    "List of Sectors\n"
+    "List of Igroup Name\n"
+    "List of Isubgroup Name\n"
+    "All the companies in particular sector\n\n"
+    "Type in `-help` to know the syntax/ command",
+    )
  
 @Bot.command()
-async def helpdetials(ctx):
-      await ctx.send("**Lets Visualise**\n"
-      "To get started you got to be ready with **Security Codes** of stocks!\n"
-      "You can easily get the Id using `-securitycode security_id` command\n\n"
-      "**Stock Charts**\n"
-      "Price Action chart- Line Graph\n"
-      "Price Action chart- Candles Graph\n"
-      "Volumn in Daily Timeframe\n"
-      "% Deli. Qty to Traded Qty\n"
-      "Daily percentage Change in price\n"
-      "Cumulative returns from the Day you bought\n\n"
-      "`For e.g- $candles 500002 23/8/21`"
-      "**Indices**\n"
-      "Indices Code using `indices` command\n"
-      "Index Price Action- Candles\n\n"
-      "Type in `-help` to know the syntax/ command\n"
-      "For the charts with Secuirty Code you need to specify the starting date\n"
-      "Cherry on the Cake- for the candles yo can also pass the Moving Average (Default=2)",
-      )
+async def helpcharts(ctx):
+    await ctx.send("**Lets Visualise**\n"
+    "To get started you got to be ready with **Security Codes** of stocks!\n"
+    "You can easily get the Id using `-securitycode security_id` command\n\n"
+    "**Stock Charts**\n"
+    "Price Action chart- Line Graph\n"
+    "Price Action chart- Candles Graph\n"
+    "Volumn in Daily Timeframe\n"
+    "% Deli. Qty to Traded Qty\n"
+    "Daily percentage Change in price\n"
+    "Cumulative returns from the Day you bought\n\n"
+    "`For e.g- $candles 500002 23/8/21`"
+    "**Indices**\n"
+    "Indices Code using `indices` command\n"
+    "Index Price Action- Candles\n\n"
+    "Type in `-help` to know the syntax/ command\n"
+    "For the charts with Secuirty Code you need to specify the starting date\n"
+    "Cherry on the Cake- for the candles yo can also pass the Moving Average (Default=2)",
+    )
 
 
 @Bot.command()
